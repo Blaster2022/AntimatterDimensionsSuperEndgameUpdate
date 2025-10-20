a86048f7c94879723d7a6e6a0ca1b139c490cbde
Supersonic Seven <b.rockholz@icloud.com>
2025-10-20 13:39:09 -0400

    Update v.js
    V 1 p3

--- src/core/secret-formula/celestials/v.js
+++ src/core/secret-formula/celestials/v.js
@@ -219,7 +219,7 @@ export const v = {
       description: () => `Have ${formatInt(10)} V-Achievements`,
       effect: () => Achievements.power,
       // Base rate is 60 ECs at 20 minutes each
-      format: x => (Ra.unlocks.instantECAndRealityUpgradeAutobuyers.canBeApplied
+      format: x => (Ra.unlocks.instantECAndRealityUpgradeAutobuyers.canBeApplied || EndgameMastery(53).isBought
         ? "Instant (Ra upgrade)"
         : `${TimeSpan.fromMinutes(60 * 20 / x).toStringShort()} for full completion`),
       requirement: () => V.spaceTheorems >= 10
