570abbc73e3bdd5bed1b3b60f10d3e272d8c0270
Supersonic Seven <b.rockholz@icloud.com>
2025-11-04 17:45:25 -0500

    Update NormalAchievementsTab.vue
    Found another reference error but I seriously think this is it

--- src/components/tabs/normal-achievements/NormalAchievementsTab.vue
+++ src/components/tabs/normal-achievements/NormalAchievementsTab.vue
@@ -74,7 +74,7 @@ export default {
       const gameSpeedupFactor = getGameSpeedupFactor();
       this.achievementPower = Achievements.power;
       this.achTPEffect = RealityUpgrade(8).config.effect();
-      this.achCountdown = Achievements.timeToNextAutoAchieve.div(gameSpeedupFactor);
+      this.achCountdown = new Decimal(Achievements.timeToNextAutoAchieve).div(gameSpeedupFactor);
       this.totalCountdown = new Decimal((Achievements.preReality.countWhere(a => !a.isUnlocked) - 1) * Achievements.period +
         Achievements.timeToNextAutoAchieve).div(gameSpeedupFactor);
       this.missingAchievements = Achievements.preReality.countWhere(a => !a.isUnlocked);
