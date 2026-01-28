7d13c0d1948b359dd26f63812d29f6b787b094b9
Supersonic Seven <b.rockholz@icloud.com>
2026-01-27 19:16:40 -0500

    Revert "Fix Charged Infinity Upgrades always discharge"

--- src/components/tabs/infinity-upgrades/InfinityUpgradesTab.vue
+++ src/components/tabs/infinity-upgrades/InfinityUpgradesTab.vue
@@ -14,7 +14,6 @@ export default {
     return {
       isDoomed: false,
       isUseless: false,
-      alwaysRecpec: false,
       chargeUnlocked: false,
       totalCharges: 0,
       chargesUsed: 0,
@@ -64,8 +63,8 @@ export default {
       return {
         "o-primary-btn--subtab-option": true,
         "o-primary-btn--charged-respec-active": this.disCharge ||
-          this.alwaysRecpec, // See src/core/celestials/pelle/pelle.js armageddon(...)
-        "o-pelle-disabled-pointer": this.alwaysRecpec
+          this.isDoomed, // Only visual effect, Discharge Upgrades on Armageddon is unaffected by Ra's logic
+        "o-pelle-disabled-pointer": this.isDoomed
       };
     },
     offlineIpUpgrade: () => InfinityUpgrade.ipOffline
@@ -86,7 +85,6 @@ export default {
     update() {
       this.isDoomed = Pelle.isDoomed;
       this.isUseless = Pelle.isDoomed && !PelleCelestialUpgrade.raTeresa2.isBought;
-      this.alwaysRecpec = this.isDoomed && Pelle.isAlwaysDischargeCIU;
       this.chargeUnlocked = Ra.unlocks.chargedInfinityUpgrades.canBeApplied && !this.isUseless;
       this.totalCharges = Ra.totalCharges;
       this.chargesUsed = Ra.totalCharges - Ra.chargesLeft;
@@ -145,13 +143,13 @@ export default {
       Charged Infinity Upgrades have their effect altered.
       <br>
       Hold shift to show Charged Infinity Upgrades.
-      <span v-if="!isDoomed || !alwaysRecpec"> You can freely respec your choices on Reality.</span>
+      <span v-if="!isDoomed"> You can freely respec your choices on Reality.</span>
     </div>
     <div v-if="isUseless">
       You cannot Charge Infinity Upgrades while Doomed.
     </div>
-    <div v-if="isDoomed && !isUseless && alwaysRecpec">
-      Charged Infinity Upgrades always reset on Armageddon, until you can keep your Break Infinity Upgrades on Armageddon.
+    <div v-if="isDoomed && !isUseless">
+      Charged Infinity Upgrades always reset on Armageddon.
     </div>
     <br>
     Within each column, the upgrades must be purchased from top to bottom.
