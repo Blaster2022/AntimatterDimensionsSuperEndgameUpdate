270eb904c6ee673b0a75e312f8fc301551249116
blaster2022 <105985545+Blaster2022@users.noreply.github.com>
2026-03-31 16:02:14 -0400

    DP Uncapping fixed (hopefully)

--- src/game.js
+++ src/game.js
@@ -1168,7 +1168,8 @@ export function gainedCelestialPoints() {
 
 export function gainedDoomedParticles() {
   if (!player.break2) return DC.D1;
-  let dp = (player.celestials.pelle.records.totalEndgameAntimatter.add(1).log10().div(9e15));
+  let dp = player.celestials.pelle.records.totalEndgameAntimatter.add(1).log10().div(9e15)
+  : Decimal.min(player.celestials.pelle.records.totalEndgameAntimatter.add(1).log10().div(9e15), new Decimal(1e100 - player.endgame.doomedParticles.toNumber()));
   return dp.floor();
 }
 
