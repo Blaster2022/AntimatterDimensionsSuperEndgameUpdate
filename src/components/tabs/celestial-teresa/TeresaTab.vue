1ab1a9114b079f1e9a0629fbc849eec6220280c2
Supersonic Seven <b.rockholz@icloud.com>
2026-08-27 13:37:21 -0400

    Refactor lastMachinesString logic for clarity

--- src/components/tabs/celestial-teresa/TeresaTab.vue
+++ src/components/tabs/celestial-teresa/TeresaTab.vue
@@ -97,9 +97,11 @@ export default {
       return GameDatabase.celestials.descriptions[0].effects();
     },
     lastMachinesString() {
-      return this.lastMachines.lt(DC.E10000)
-        ? `${quantify("Reality Machine", this.lastMachines, 2)}`
-        : `${quantify("Imaginary Machine", this.lastMachines.dividedBy(DC.E10000), 2)}`;
+      return this.lastMachines.gte(DC.E20000)
+        ? `${quantify("Eterigary Machine", this.lastMachines.dividedBy(DC.E20000), 2)}`
+        : (this.lastMachines.lt(DC.E10000)
+          ? `${quantify("Reality Machine", this.lastMachines, 2)}`
+          : `${quantify("Imaginary Machine", this.lastMachines.dividedBy(DC.E10000), 2)}`);
     },
     unlockInfoTooltipArrowStyle() {
       return {
