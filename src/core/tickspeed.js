1c598a488205da2848cebda29d8107eb82ff625d
Supersonic Seven <b.rockholz@icloud.com>
2026-09-10 17:34:48 -0400

    Update remnant galaxy calculation logic
    Refactor remnant galaxy calculation to use Decimal methods.

--- src/core/tickspeed.js
+++ src/core/tickspeed.js
@@ -60,7 +60,7 @@ export function getTickSpeedMultiplier() {
   galaxies = galaxies.times(getAdjustedGlyphEffect("realitygalaxies"));
   galaxies = galaxies.times(1 + ImaginaryUpgrade(9).effectOrDefault(0));
   if (Pelle.isDoomed && !PelleDestructionUpgrade.disableGalaxyNerf.isBought) galaxies = galaxies.times(0.5);
-  if (Pelle.isDoomed && EndgameMilestone.remnantGalaxy.isReached) galaxies = galaxies.times(Decimal.pow(1 + Math.log10(Currency.remnants.value + 1), 0.5));
+  if (Pelle.isDoomed && EndgameMilestone.remnantGalaxy.isReached) galaxies = galaxies.times(Decimal.pow(Decimal.log10(Currency.remnants.value.add(1)).add(1), 0.5));
   if (GalacticPowers.galaxyStrength.isUnlocked) galaxies = galaxies.times(GalacticPowers.galaxyStrength.reward);
   galaxies = galaxies.timesEffectOf(EterigaryUpgrade(9));
 
