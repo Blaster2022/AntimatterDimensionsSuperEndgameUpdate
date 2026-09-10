6df5c61582bda6502adfaf09428da04fd7c50678
Supersonic Seven <b.rockholz@icloud.com>
2026-09-10 17:20:12 -0400

    Expand GalaxyGeneratorAutobuyer with new upgrades

--- src/core/autobuyers/galaxy-generator-autobuyer.js
+++ src/core/autobuyers/galaxy-generator-autobuyer.js
@@ -1,14 +1,16 @@
 import { IntervaledAutobuyerState } from "./autobuyer";
 
 export class GalaxyGeneratorAutobuyerState extends IntervaledAutobuyerState {
-  get _upgradeName() { return ["additive", "multiplicative", "antimatterMult", "IPMult", "EPMult", "RSMult"][this.id - 1]; }
+  get _upgradeName() {
+    return ["additive", "multiplicative", "antimatterMult", "IPMult", "EPMult", "RSMult", "DTMult", "remnantPow", "exponential", "superExponential"][this.id - 1];
+  }
 
   get data() {
     return player.auto.galaxyGenerator.all[this.id - 1];
   }
 
   get name() {
-    return ["Base Galaxy Multiplier", "Multiplicative Galaxy Multiplier", "Antimatter Multiplier", "Infinity Point Multiplier", "Eternity Point Multiplier", "Reality Shard Multiplier"][this.id - 1];
+    return ["Base Galaxy Multiplier", "Multiplicative Galaxy Multiplier", "Antimatter Multiplier", "Infinity Point Multiplier", "Eternity Point Multiplier", "Reality Shard Multiplier", "Dilated Time Multiplier", "Remnant Power", "Galaxy Power", "Galaxy Dilation"][this.id - 1];
   }
 
   get interval() {
@@ -16,6 +18,10 @@ export class GalaxyGeneratorAutobuyerState extends IntervaledAutobuyerState {
   }
 
   get isUnlocked() {
+    if (this.id === 10) return player.celestials.ra.pets.pelle.level >= 24;
+    if (this.id === 9) return player.celestials.ra.pets.pelle.level >= 18;
+    if (this.id === 8) return player.celestials.ra.pets.pelle.level >= 12;
+    if (this.id === 7) return player.celestials.ra.pets.pelle.level >= 6;
     return ExpansionPack.pellePack.isBought;
   }
 
@@ -31,11 +37,11 @@ export class GalaxyGeneratorAutobuyerState extends IntervaledAutobuyerState {
     if (Pelle.hasGalaxyGenerator) {
       super.tick();
       const upgradeName = this._upgradeName;
-      GalaxyGeneratorUpgrades[upgradeName].purchase(this.bulk);
+      GalaxyGeneratorUpgrades[upgradeName].purchase(false);
     }
   }
 
-  static get entryCount() { return 6; }
+  static get entryCount() { return 10; }
   static get autobuyerGroupName() { return "Galaxy Generator Upgrade"; }
   static get isActive() { return player.auto.galaxyGenerator.isActive; }
   static set isActive(value) { player.auto.galaxyGenerator.isActive = value; }
